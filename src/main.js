ff8aa6050b128f57035d0f3590862ff0de7f556c
QueWenHao <541578749@qq.com>
2018-10-09 16:39:27 +0800

    首页组件创建成功

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router';
+
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+import index from './components/index.vue';
+
+
+//写路由规则
+
+const routes = [
+
+  {
+    path: '/',
+    component: index
+  },
+
+  {
+    path: '/index',
+    component: index
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
+
 }).$mount('#app')
